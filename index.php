bca73c08dd32b166fb5bf591cdcfbda9ebfe0112
crxamm <6421061011@cdti.ac.th>
2023-06-21 11:19:43 +0700

    first

---
+++ index.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="style.css" />
+<?php
+
+$name = "Sukritta Chinplikanont";
+$age = 18;
+$year = 2566;
+$byear = $year - $age;
+
+echo "<h1><center> Welocome </center></h1><b>";
+echo "<center><font color='	LightSalmon'>by $name | <font color='SlateBlue'>born on $byear</center></br>";
+echo "<center><img src='image/sawako2.gif'</center> </br></br>";
+
+?>
+<p id="link"> <a href= 'login.php'> Login </p>
---
+++ login.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="style.css" />
+
+<div id="box1";>
+    Login
+</div>
+</br>
+<center>
+    Username :
+    <input name = "username" type = "text"/><br/><br/>
+    Password :
+    <input pass = "password" type = "password"/><br/><br/>
+    <input type = "submit" value = "login"/>
+<center>
+</br>
+<?php
+echo "<center><img src='image/chin3.jpg'</center> </br></br>";
+?>
+
+<center>
+    <p id="link2"> <a href= 'index.php'> Go Back to Homepage </p>
+<center>
